e6f56b681a13644fbb8db9fe6661d1e1b00afda9
okifu <okifurhere@gmail.com>
2024-12-19 23:39:54 +0800

    a

---
+++ src/solver/contact_solver.cu
@@ -0,0 +1,8 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/for_each.h>
+#include "mutils/common_types.h"
+
+using namespace  ADU;
+
